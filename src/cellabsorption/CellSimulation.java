c932d7bccfb8c1b4a3356a7fe95ca6aea5f4a9c2
Bavo27 <bvandenh@macalester.edu>
2024-10-04 18:38:42 -0400

    Absorb Finish(Full completion)

--- src/cellabsorption/CellSimulation.java
+++ src/cellabsorption/CellSimulation.java
@@ -28,7 +28,7 @@ public class CellSimulation {
             Point canvasCenter = new Point(canvas.getWidth() / 2.0, canvas.getHeight() / 2.0);
             for (Cell cell : cells){
             cell.moveAround(canvasCenter);
-            cell.grow(0.02);
+            handleCellInteraction();
             }
             canvas.draw();
             canvas.pause(10);
@@ -46,6 +46,16 @@ public class CellSimulation {
             Color.getHSBColor(rand.nextFloat(), rand.nextFloat() * 0.5f + 0.1f, 1));
             canvas.add(cell.getShape());
             cells.add(cell);
+        }
+    }
+    private void handleCellInteraction() {
+        for (int i = 0; i < cells.size(); i++) {
+            Cell cell0 = cells.get(i);
+            for (int j = i + 1; j < cells.size(); j++) {
+                Cell cell1 = cells.get(j);
+                cell0.interactWith(cell1);
+                // TODO: insert call here to make cell0 interact with cell1
+            }
         }
     }
 }
